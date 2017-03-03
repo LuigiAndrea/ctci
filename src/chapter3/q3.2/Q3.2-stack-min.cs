2d85ae3c85efab1c1a8d1ea769aaf356c44d5985
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-03-03 17:59:00 -0500

    Q3.2-stack-min source code

---
+++ src/chapter3/q3.2/Q3.2-stack-min.cs
@@ -0,0 +1,38 @@
+// Stack Min: How would you design a stack which, in addition to push and pop, has a function min
+// which returns the minimum element? Push, pop and min should all operate in 0(1) time.
+
+namespace Chapter3
+{
+    public class Q3_2StackMin
+    {
+        public class StackWithMin : Stack<int>
+        {
+            Stack<int> stackForMin = new Stack<int>();
+
+            new public void push(int item)
+            {
+                if (item <= min())
+                {
+                    stackForMin.push(item);
+                }
+                base.push(item);
+            }
+
+            new public int pop()
+            {
+                int value = base.pop();
+                if (value == min())
+                    stackForMin.pop();
+
+                return value;
+            }
+
+            public int min()
+            {
+                return (stackForMin.isEmpty())
+                        ? int.MaxValue
+                        : stackForMin.peek();
+            }
+        }
+    }
+}

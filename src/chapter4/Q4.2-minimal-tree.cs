5bcf0205f4e79801526410f0d0a01e560bf1a8a9
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-03-22 17:19:56 -0400

    Added problem Q4.2

---
+++ src/chapter4/Q4.2-minimal-tree.cs
@@ -0,0 +1,23 @@
+//Minimal Tree: Given a sorted (increasing  order) array with unique integer elements, 
+//write an algorithm to create a binary search tree with minimal height. 
+
+namespace Chapter4
+{
+    public static class Q4_2MinimalTree
+    {
+        public static TreeBinaryNode<int> MinimalTree(int[] array)
+        {
+            return MinimalTree(array, 0, array.Length - 1);
+        }
+
+        private static TreeBinaryNode<int> MinimalTree(int[] array, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            int mid = (start + end) / 2;
+            return new TreeBinaryNode<int>(array[mid], MinimalTree(array, start, mid - 1), MinimalTree(array, mid + 1, end));
+        }
+    }
+
+}

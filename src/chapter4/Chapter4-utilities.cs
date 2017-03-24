507e1a29bbc7b928245e0541d9a5e8646058fcea
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-03-23 21:18:20 -0400

    Make generic Traversal Utility Chapter 4

--- src/chapter4/Chapter4-utilities.cs
+++ src/chapter4/Chapter4-utilities.cs
@@ -1,36 +1,59 @@
-using static System.Console;
+using System.Collections.Generic;
 
 namespace Chapter4
 {
     public static class Utilities
     {
-        public static void inOrderTraversal(TreeBinaryNode<int> node)
+        public static class Traversal<T>
         {
-            if (node != null)
-            {
-                inOrderTraversal(node.left);
-                WriteLine(node.value);
-                inOrderTraversal(node.right);
-            }
-        }
+            public enum typeTraversal { preOrder, inOrder, postOrder }
+            static List<T> listOrder = new List<T>();
 
-        public static void preOrderTraversal(TreeBinaryNode<int> node)
-        {
-            if (node != null)
+            public static List<T> getTraversalList(typeTraversal type, TreeBinaryNode<T> t)
             {
-                WriteLine(node.value);
-                preOrderTraversal(node.left);
-                preOrderTraversal(node.right);
-            }
-        }
+                switch ((int)type)
+                {
+                    case 0:
+                        preOrderTraversal(t);
+                        break;
+                    case 1:
+                        inOrderTraversal(t);
+                        break;
+                    case 2:
+                        postOrderTraversal(t);
+                        break;
+                }
 
-        public static void postOrderTraversal(TreeBinaryNode<int> node)
-        {
-            if (node != null)
+                return listOrder;
+            }
+            private static void inOrderTraversal(TreeBinaryNode<T> node)
             {
-                postOrderTraversal(node.left);
-                postOrderTraversal(node.right);
-                WriteLine(node.value);
+                if (node != null)
+                {
+                    inOrderTraversal(node.left);
+                    listOrder.Add(node.value);
+                    inOrderTraversal(node.right);
+                }
+            }
+
+            private static void preOrderTraversal(TreeBinaryNode<T> node)
+            {
+                if (node != null)
+                {
+                    listOrder.Add(node.value);
+                    preOrderTraversal(node.left);
+                    preOrderTraversal(node.right);
+                }
+            }
+
+            private static void postOrderTraversal(TreeBinaryNode<T> node)
+            {
+                if (node != null)
+                {
+                    postOrderTraversal(node.left);
+                    postOrderTraversal(node.right);
+                    listOrder.Add(node.value);
+                }
             }
         }
     }

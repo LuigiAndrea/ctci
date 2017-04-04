70f9028c7e102eaa8c0a391b7ec18a925acccf87
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-04-04 12:17:33 -0400

    added tests for list-of-depths-iterative Q4.3

--- test/chapter4/Q4.3-list-of-depths-test.cs
+++ test/chapter4/Q4.3-list-of-depths-test.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Xunit;
 
 using Chapter4;
-using static Chapter4.Q4_3ListOfDepths;
 using static Chapter4.Q4_2MinimalTree;
 
 namespace Tests.Chapter4
@@ -14,7 +13,8 @@ namespace Tests.Chapter4
         private void ListOfDepthsTest()
         {
             TreeBinaryNode<int> tree = createTree();
-            List<LinkedList<TreeBinaryNode<int>>> l = getListOfDepths<int>(tree) ?? new List<LinkedList<TreeBinaryNode<int>>>();
+            List<LinkedList<TreeBinaryNode<int>>> l = Q4_3ListOfDepths.getListOfDepths<int>(tree);
+            List<LinkedList<TreeBinaryNode<int>>> l2 = Q4_3ListOfDepthsIterative.getListOfDepths<int>(tree);
             int[][] result = {
                 new int[1]{5},
                 new int[2]{2,8},
@@ -22,21 +22,17 @@ namespace Tests.Chapter4
                 new int[4]{1,4,7,10}
             };
 
-            for (int i = 0; i < l.Count; i++)
-            {
-                int j = 0;
-                foreach (TreeBinaryNode<int> linkedListNode in l[i])
-                {
-                    Assert.True(linkedListNode.value.Equals(result[i][j++]));
-                }
-            }
+            checkValues(l, result);
+            checkValues(l2, result);
         }
 
         [FactAttribute]
         private void ListOfDepthsNullTest()
         {
-            List<LinkedList<TreeBinaryNode<int>>> list = getListOfDepths<int>(null);
+            List<LinkedList<TreeBinaryNode<int>>> list = Q4_3ListOfDepths.getListOfDepths<int>(null);
             Assert.True(list.Count.Equals(0));
+            List<LinkedList<TreeBinaryNode<int>>> list2 = Q4_3ListOfDepthsIterative.getListOfDepths<int>(null);
+            Assert.True(list2.Count.Equals(0));
         }
 
         private TreeBinaryNode<int> createTree()
@@ -49,6 +45,18 @@ namespace Tests.Chapter4
             }
 
             return MinimalTree(array);
+        }
+
+        private void checkValues<T>(List<LinkedList<TreeBinaryNode<T>>> l, T[][] result)
+        {
+            for (int i = 0; i < l.Count; i++)
+            {
+                int j = 0;
+                foreach (TreeBinaryNode<T> linkedListNode in l[i])
+                {
+                    Assert.True(linkedListNode.value.Equals(result[i][j++]));
+                }
+            }
         }
     }
 }

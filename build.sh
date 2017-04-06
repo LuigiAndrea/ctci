1b3069408ad0ffa0112d1c7c9cb3e0808f3c4838
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-04-06 00:59:43 -0400

    CI updates

--- build.sh
+++ build.sh
@@ -9,9 +9,7 @@ if [ -d $artifactsFolder ]; then
   rm -R $artifactsFolder
 fi
 
-cd test
-dotnet restore
-dotnet test
+cd test; dotnet restore; dotnet test
 
 revision=${TRAVIS_JOB_ID:=1}  
 revision=$(printf "%04d" $revision) 

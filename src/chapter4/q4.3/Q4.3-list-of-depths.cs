43716918b4c222405376a823dc593f8c6308f5bc
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-04-10 01:26:21 -0400

    Added README in Q4.3

--- src/chapter4/q4.3/Q4.3-list-of-depths.cs
+++ src/chapter4/q4.3/Q4.3-list-of-depths.cs
@@ -1,6 +1,3 @@
-// List of Depths: Given a binary tree, design an algorithm which creates a linked list of all the nodes 
-// at each depth (e.g., if you have a tree with depth D, you'll have D linked lists)
-
 using System.Collections.Generic;
 
 namespace Chapter4

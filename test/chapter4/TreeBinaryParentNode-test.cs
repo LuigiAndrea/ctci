d056e7bdeb7c2dff80936dee7db83420990b13d9
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-05-26 02:44:59 -0400

    test: add test for TreeBinaryParentNode

---
+++ test/chapter4/TreeBinaryParentNode-test.cs
@@ -0,0 +1,24 @@
+using Xunit;
+
+using Chapter4;
+using static Tests.Chapter4.TestUtilities;
+
+namespace Tests.Chapter4
+{
+    public class TreeBinaryParentNodeTest
+    {
+        [FactAttribute]
+        private static void TreeBinaryParentNodeBuildingTest()
+        {
+            int[] array = new int[5] { 1, 5, 12, 4, 14 };
+            TreeBinaryParentNode<int> tree = buildParentTree.MinimalParentTree(array);
+            TreeBinaryParentNode<int> child = (TreeBinaryParentNode<int>)tree.right.right;
+
+            Assert.Equal(child.parent.value,4);
+            Assert.Equal(tree.left.value,1);
+            Assert.Null(tree.left.left);
+            Assert.Null(child.parent.left);
+            Assert.Equal(child.parent.right.value,14);
+        }
+    }
+}

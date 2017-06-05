a19b157522f291167a2e9418245a272fa0c18c0c
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-06-05 12:35:41 -0400

    test: add tests for the iterative version of successor (Q4.6)

--- test/chapter4/Q4.6-successor-test.cs
+++ test/chapter4/Q4.6-successor-test.cs
@@ -2,6 +2,7 @@ using Xunit;
 
 using Chapter4;
 using static Chapter4.Q4_6Successor<int>;
+using static Chapter4.Q4_6SuccessorIterative<int>;
 using static Tests.Chapter4.TestUtilities;
 
 namespace Tests.Chapter4
@@ -11,7 +12,7 @@ namespace Tests.Chapter4
         [FactAttribute]
         private static void TreeBinaryParentNodeBuildingTest()
         {
-            int[] array = new int[7] { 1, 5, 12, 4, 14, 28, 33 };          
+            int[] array = new int[7] { 1, 5, 12, 4, 14, 28, 33 };
 
             TreeBinaryParentNode<int> tree = buildParentTree.MinimalParentTree(array);
             TreeBinaryParentNode<int> n = (TreeBinaryParentNode<int>)tree.left.right;
@@ -20,12 +21,24 @@ namespace Tests.Chapter4
             Assert.Equal(12, s.value);
             Assert.Equal(n, s);
 
+            TreeBinaryParentNode<int> sIt = successorIt(n.parent);
+            Assert.Equal(12, sIt.value);
+            Assert.Equal(n, sIt);
+
             TreeBinaryParentNode<int> sn = successor(n);
             Assert.Equal(4, sn.value);
             Assert.Equal(sn, tree);
-            
-            Assert.Null(successor((TreeBinaryParentNode<int>)tree.right.right));
+
+            TreeBinaryParentNode<int> snIt = successorIt(n);
+            Assert.Equal(4, snIt.value);
+            Assert.Equal(snIt, tree);
+
+            TreeBinaryParentNode<int> lastNode = (TreeBinaryParentNode<int>)tree.right.right;
+            Assert.Null(successor(lastNode));
             Assert.Null(successor(null));
+
+            Assert.Null(successorIt(lastNode));
+            Assert.Null(successorIt(null));
         }
     }
 }

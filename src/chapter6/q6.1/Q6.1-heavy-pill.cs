d806cbae19c091391f5f93a6bb4111ab9bf5e0e5
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-07-04 03:52:47 -0400

    feat: add Q6.1 The Heavy Pill

---
+++ src/chapter6/q6.1/Q6.1-heavy-pill.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Text;
+using static System.Console;
+
+namespace Chapter6
+{
+    public static class Q6_1HeavyPill
+    {
+        private static double totalNormalWeight;
+        private static double diffWeight;
+        private static ((double normalWeight, double specialWeight) pill, int count) bottle;
+
+        public static void init(((double n, double s) p, int count) b)
+        {
+            bottle = b;
+            diffWeight = bottle.pill.specialWeight - bottle.pill.normalWeight;
+
+            if (b.count < 2 & diffWeight==0)
+                throw new ArgumentException();        
+            
+            totalNormalWeight = (bottle.count * (bottle.count + 1) / 2) * bottle.pill.normalWeight;
+            WriteLine("The bottle # {0} has the {1} {2}",getBottle(),diffWeight>0 ? "heavy" : "light", "pills");
+        }
+        public static double getBottle()
+        {
+            return Math.Round((scale() - totalNormalWeight) / diffWeight, 0);
+        }
+
+        private static double scale()
+        {
+            Random r = new Random();
+            int n = r.Next(1, bottle.count);
+            WriteLine($"Random bottle chosen: {n}");
+            return totalNormalWeight + n * diffWeight;
+        }
+    }
+}

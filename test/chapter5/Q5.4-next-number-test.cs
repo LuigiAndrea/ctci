fa7b777bd088333bd2b522e4adbb6f07b6154ed6
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-07-17 12:29:12 -0400

    test: add test for Q4.5

--- test/chapter5/Q5.4-next-number-test.cs
+++ test/chapter5/Q5.4-next-number-test.cs
@@ -9,27 +9,21 @@ namespace Tests.Chapter5
         [FactAttribute]
         private static void NextNumberTest()
         {
-            int s; int l;
-            (s, l) = nextNumber(287);
-            Assert.Equal((s, l), (252, 303));
-            (s, l) = nextNumber(4);
-            Assert.Equal((s, l), (2, 8));
-            (s, l) = nextNumber(113);
-            Assert.Equal((s, l), (108, 114));
-            (s, l) = nextNumber(56);
-            Assert.Equal((s, l), (52, 67));
-            (s, l) = nextNumber(int.MaxValue);
-            Assert.Equal((s, l), (-1, -1));
-            (s, l) = nextNumber(int.MaxValue - 1);
-            Assert.Equal((s, l), (int.MaxValue-2, -1));
-            (s, l) = nextNumber(int.MaxValue - 2);
-            Assert.Equal((s, l), (int.MaxValue-4, int.MaxValue-1));
+            (int input, int out1, int out2)[] cases = {
+                (287, 252, 303),
+                (4, 2, 8),
+                (113, 108, 114),
+                (56, 52, 67),
+                (int.MaxValue,-1,-1),
+                (int.MaxValue-1,int.MaxValue-2,-1),
+                (int.MaxValue-1,int.MaxValue-2,-1),
+                (int.MaxValue-2,int.MaxValue-4,int.MaxValue-1)};
 
-
-            // (int s2, int l2) = nextNumber(56);
-            // (int s2, int l2) = nextNumber(int.MaxValue);
-            // WriteLine($"{s} - > {l}");
-            // WriteLine($"{s2} - > {l2}");
+            foreach (var test in cases)
+            {
+                Assert.Equal((test.out1, test.out2), nextNumber(test.input));
+                Assert.Equal((test.out1, test.out2), nextNumber2(test.input));
+            }
         }
     }
 }

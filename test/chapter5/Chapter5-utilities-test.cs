155baa9d856eaa61bf6bed319ab1239b2cd4b02d
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-07-18 16:58:26 -0400

    test: add test for chapter5-utilities

---
+++ test/chapter5/Chapter5-utilities-test.cs
@@ -0,0 +1,27 @@
+using Xunit;
+
+using static Chapter5.Utilities;
+
+namespace Tests.Chapter5
+{
+    public class Chapter5UtilitiesTest
+    {
+        [FactAttribute]
+        private static void UtilitiesTest()
+        {
+            int number = 100;
+            int index = 3;
+            Assert.Equal(false, getBit(number, index));
+            Assert.Equal(108, setBit(number, index));
+
+            Assert.Equal(108, updateBit(number, index, true));
+            Assert.Equal(100, updateBit(number, index, false));
+
+            Assert.Equal(108, flipBit(number, index));
+            
+            Assert.Equal(100, clearBit(number, index));
+            Assert.Equal(4, clearFromMostSignificantBitThroughIndex(number, index));
+            Assert.Equal(96, clearFromIndexThroughZero(number, index));
+        }
+    }
+}

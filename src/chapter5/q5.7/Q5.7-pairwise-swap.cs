8f631bbae3da696593ee8baa577b5baf3f82e4e9
Luigi Andrea D'Aversa <luigiandrea@gmail.com>
2017-07-27 13:38:20 -0400

    test: add test for Q5.7

--- src/chapter5/q5.7/Q5.7-pairwise-swap.cs
+++ src/chapter5/q5.7/Q5.7-pairwise-swap.cs
@@ -4,9 +4,7 @@ namespace Chapter5
 {
     public static class Q5_7PairwiseSwap
     {
-        public static int pairwiseSwap(int number)
-        {
-            return ((int)((uint)0xaaaaaaaa & number) >>1) | ((0x55555555 & number) << 1) ;
-        }
+        public static int pairwiseSwap(int number) 
+                    => (int)(((uint)0xaaaaaaaa & number) >> 1) | ((0x55555555 & number) << 1);
     }
 }
---
+++ test/chapter5/Q5.7-pairwise-swap.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+using static Chapter5.Q5_7PairwiseSwap;
+
+namespace Tests.Chapter5
+{
+    public class Q5_7
+    {
+        [FactAttribute]
+        private static void PairWiseTest()
+        {
+            (int input, int output)[] cases = {
+                (3726,3405),
+                (int.MaxValue,unchecked((int)0xbfffffff)),
+               (int.MinValue,0x40000000),
+                (0,0),
+                (1,2),
+                };
+
+            foreach (var test in cases)
+                Assert.Equal(test.output, pairwiseSwap(test.input));
+
+        }
+    }
+}

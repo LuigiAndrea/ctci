79e33619d63a7f9193db0c8036df1ba606ba5af7
LAD <luigiandrea@gmail.com>
2018-08-23 22:31:22 -0400

    Added Q8.2 Robot in a grid

---
+++ src/chapter8/q8.2/Q8.2-robot-grid.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Chapter8
+{
+    public static class Q8_2RobotGrid
+    {
+        private static List<Point> Points = new List<Point>();
+        public static List<Point> getPathRobot(bool[,] grid)
+        {
+            (int colms, int rows) gridDim = getDimensions(grid);
+            bool res = getPath(gridDim, grid);
+
+            return (res) ? Points : throw new ArgumentException("The maze must have a path that leads to the bottom right!");
+        }
+
+        private static bool getPath((int c, int r) gridDim, bool[,] grid)
+        {
+            if (offLimit(gridDim, grid))
+                return false;
+
+            if (reachedBottom(gridDim) || getPath((gridDim.c - 1, gridDim.r), grid) || getPath((gridDim.c, gridDim.r - 1), grid))
+            {       
+                Points.Add(new Point(){column = gridDim.c, row =gridDim.r}) ;
+                return true;
+            }
+
+            return false;
+        }
+
+        private static bool reachedBottom((int cols, int rows) gridDim) => gridDim.rows == 0 && gridDim.cols == 0;
+        private static bool offLimit((int cols, int rows) gridDim, bool[,] grid) => gridDim.rows < 0 || gridDim.cols < 0 || !grid[gridDim.cols, gridDim.rows];
+        private static (int c, int r) getDimensions(bool[,] grid) => (grid.GetLength(0) - 1, grid.GetLength(1) - 1);
+
+        public class Point {
+            public int column;
+            public int row;
+        }
+    }
+}

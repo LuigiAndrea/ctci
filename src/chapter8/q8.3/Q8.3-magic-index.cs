b046f35fbaa6b221e758aec0febfc240310a6fe3
LAD <luigiandrea@gmail.com>
2018-12-01 01:49:21 -0500

    add q8_3, magic-index without distinct values

--- src/chapter8/q8.3/Q8.3-magic-index.cs
+++ src/chapter8/q8.3/Q8.3-magic-index.cs
@@ -6,9 +6,9 @@ namespace Chapter8
     {
         public static int findMagicIndex(int[] magicArray)
         {
-            if(magicArray == null || magicArray.Length == 0)
+            if (magicArray == null || magicArray.Length == 0)
                 throw new ArgumentNullException(paramName: $"{nameof(magicArray)} must not be null or empty");
-            int magicIndex = findIndex(0, magicArray.Length-1, magicArray);
+            int magicIndex = findIndex(0, magicArray.Length - 1, magicArray);
             return magicIndex;
         }
 
@@ -18,17 +18,54 @@ namespace Chapter8
                 return -1;
 
             int magicIndex = -1;
-            int position = (leftIndex + rightIndex) /2;
-            
-            if(magicArray[position] == position)
-                magicIndex = position;              
-            else if(magicArray[position] > position)
-                magicIndex = findIndex(leftIndex, position-1,magicArray);
-            else if(magicArray[position] < position)
-                magicIndex = findIndex(position+1, rightIndex,magicArray);
-            
+            int position = (leftIndex + rightIndex) / 2;
+
+            if (magicArray[position] == position)
+                magicIndex = position;
+            else if (magicArray[position] > position)
+                magicIndex = findIndex(leftIndex, position - 1, magicArray);
+            else if (magicArray[position] < position)
+                magicIndex = findIndex(position + 1, rightIndex, magicArray);
+
             return magicIndex;
 
+        }
+
+        public static int findMagicIndexNotDistinctValues(int[] magicArray)
+        {
+            if (magicArray == null || magicArray.Length == 0)
+                throw new ArgumentNullException(paramName: $"{nameof(magicArray)} must not be null or empty");
+            int magicIndex = findIndexNotDistinct(0, magicArray.Length - 1, magicArray);
+            return magicIndex;
+        }
+
+        private static int findIndexNotDistinct(int leftIndex, int rightIndex, int[] magicArray)
+        {
+            int magicIndex = -1;
+
+            if (leftIndex > rightIndex)
+                return magicIndex;
+
+            int magicLength = magicArray.Length;
+            int pos = (leftIndex + rightIndex) / 2;
+            int magicValue = magicArray[pos];
+
+            if (magicValue == pos)
+                return magicValue;
+
+            int newRightIndex = Math.Min(magicValue, pos - 1);
+            if (newRightIndex >= 0)
+                magicIndex = findIndexNotDistinct(leftIndex, newRightIndex, magicArray);
+
+            if (magicIndex == -1)
+            {
+                int newLeftIndex = Math.Max(magicValue, pos + 1);
+
+                if (newLeftIndex < magicLength)
+                    return findIndexNotDistinct(newLeftIndex, rightIndex, magicArray);
+            }
+
+            return magicIndex;
         }
     }
 }
--- test/chapter8/Q8.3-magic-index-test.cs
+++ test/chapter8/Q8.3-magic-index-test.cs
@@ -18,6 +18,9 @@ namespace Tests.Chapter8
 
             Exception actualNullException = Record.Exception(() => findMagicIndex(null));
             Assert.IsType<ArgumentNullException>(actualNullException);
+
+            actualNullException = Record.Exception(() => findMagicIndexNotDistinctValues(null));
+            Assert.IsType<ArgumentNullException>(actualNullException);
         }
 
         [FactAttribute]
@@ -25,15 +28,24 @@ namespace Tests.Chapter8
         {
             int[] magic = new int[] { -1, 0, 2, 31, 44, 45, 47, 118 };
             int magicIndex = findMagicIndex(magic);
-            Assert.Equal(magicIndex, 2);
+            Assert.Equal(2, magicIndex);
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(2, magicIndex);
 
             magic = new int[] { 0 };
             magicIndex = findMagicIndex(magic);
-            Assert.Equal(magicIndex, 0);
+            Assert.Equal(0, magicIndex);
 
-            magic = new int[] { -3, -1, 1,2,3,4,5,6,7,8,10};
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(0, magicIndex);
+
+            magic = new int[] { -3, -1, 1, 2, 3, 4, 5, 6, 7, 8, 10 };
             magicIndex = findMagicIndex(magic);
-            Assert.Equal(magicIndex, 10);
+            Assert.Equal(10, magicIndex);
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(10, magicIndex);
         }
 
         [FactAttribute]
@@ -41,7 +53,57 @@ namespace Tests.Chapter8
         {
             int[] magic = new int[] { -10, -5, 1, 13, 44, 45, 47, 118 };
             int magicIndex = findMagicIndex(magic);
-            Assert.Equal(magicIndex, -1);
+            Assert.Equal(-1, magicIndex);
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex);
+
+            magic = new int[] { -10, -9, -8, -7, -6, -5, -2, -1 };
+            magicIndex = findMagicIndex(magic);
+            Assert.Equal(-1, magicIndex);
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex);
+
+            magic = new int[] { 30, 44, 78, 97, 98, 106, 115, 122 };
+            magicIndex = findMagicIndex(magic);
+            Assert.Equal(-1, magicIndex);
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex);
+        }
+
+        [FactAttribute]
+        private static void ArrayWithoutMagicIndexNotDistinct()
+        {
+            int[] magic = new int[] { -2, -2, 1, 2, 2, 2, 2 };
+
+            int magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex); 
+            
+            magic = new int[] { 12, 12, 12, 12, 12, 12, 12, 15 };
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex); 
+
+            magic = new int[] { 1, 2, 3, 5, 5, 7, 7, 8 };
+
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(-1, magicIndex); 
+        
+        }
+
+        [FactAttribute]
+        private static void ArrayWithtMagicIndexNotDistinct()
+        {
+            int[] magic = new int[] { -1, 2, 2, 2, 3, 7, 12 };
+
+            int magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(2, magicIndex);
+
+            magic = new int[] { -3, -1, -1, -1, 3, 3, 6 };
+            magicIndex = findMagicIndexNotDistinctValues(magic);
+            Assert.Equal(6, magicIndex);
         }
     }
 }

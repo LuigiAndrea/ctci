9b74b8170b59283b58f3799dc31ff8084d3deb57
LAD <luigiandrea@gmail.com>
2018-12-06 22:26:59 -0500

    add small and big numbers in the call to calculate_product Reduce computation for Q8.5 first veriosn

--- src/chapter8/q8.5/Q8.5-recursive-multiply.cs
+++ src/chapter8/q8.5/Q8.5-recursive-multiply.cs
@@ -8,20 +8,13 @@ namespace Chapter8
     {
         public static int multiply(int a, int b)
         {
-            int smallerNumber = (a>b) ? b : a;
-            int biggerNumber = (a>b) ? a : b;
-            int baseCase = -1;
+            int smallerNumber = (a > b) ? b : a;
+            int biggerNumber = (a > b) ? a : b;
 
             if (a < 0 || b < 0)
                 throw new ArgumentOutOfRangeException(paramName: $"{nameof(multiply)} - the values to calculate the product must be positive");
-            else if (a == 0 || b == 0)
-                baseCase = 0;
-            else if (a == 1)
-                baseCase = b;
-            else if (b == 1)
-                baseCase = a;
 
-            return (baseCase != -1) ? baseCase : calculate_product(a, b, 0);
+            return calculate_product(smallerNumber, biggerNumber, 0);
         }
 
         private static int calculate_product(int smallerNumber, int biggerNumber, int result)

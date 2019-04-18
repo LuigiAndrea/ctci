2e2019c20065dcbcde0ae3a8651e0facdc92d765
LAD <luigiandrea@gmail.com>
2019-04-17 23:59:12 -0400

    Add second version for Q8.7 get permutation

--- src/chapter8/q8.7/Q8.7-perm-no-dups.cs
+++ src/chapter8/q8.7/Q8.7-perm-no-dups.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Chapter8
 {
@@ -7,6 +8,9 @@ namespace Chapter8
     {
         public static List<string> getPermutations(string phrase)
         {
+            if (phrase == null)
+                RaisePermutationException(nameof(getPermutations));
+
             List<string> result = new List<string>();
             if (phrase.Length == 0)
             {
@@ -18,7 +22,7 @@ namespace Chapter8
             for (int i = 0; i < phrase.Length; i++)
             {
                 string begin = phrase.Substring(0, i);
-                string end = phrase.Substring(i+1);
+                string end = phrase.Substring(i + 1);
                 partialResult = getPermutations(begin + end);
                 foreach (var item in partialResult)
                 {
@@ -28,5 +32,36 @@ namespace Chapter8
 
             return result;
         }
+
+        public static List<string> getPermutations2(string phrase)
+        {
+            if (phrase == null)
+                RaisePermutationException(nameof(getPermutations2));
+
+            List<string> perm = new List<string>();
+
+            if (phrase.Length == 0)
+            {
+                perm.Add("");
+                return perm;
+            }
+
+            string firstChar = phrase.Substring(0, 1);
+            string remainder = phrase.Substring(1);
+
+            List<string> words = getPermutations2(remainder);
+
+            foreach (var word in words)
+            {
+                for (int i = 0; i <= word.Length; i++)
+                {
+                    string newPhrase = new StringBuilder(word).Insert(i, firstChar).ToString();
+                    perm.Add(newPhrase);
+                }
+            }
+            return perm;
+        }
+
+        private static void RaisePermutationException(string method) => throw new ArgumentException($"{method}: The string to permutate must be not null");
     }
 }

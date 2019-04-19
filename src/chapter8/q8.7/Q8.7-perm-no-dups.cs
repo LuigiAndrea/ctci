04b39d82c5d6853273be78a0ebbf227a31540093
LAD <luigiandrea@gmail.com>
2019-04-19 00:32:12 -0400

    Add Q8.7 iterative version

--- src/chapter8/q8.7/Q8.7-perm-no-dups.cs
+++ src/chapter8/q8.7/Q8.7-perm-no-dups.cs
@@ -65,12 +65,10 @@ namespace Chapter8
         public static List<string> getPermutations3(string phrase)
         {
             if (phrase == null)
-                RaisePermutationException(nameof(getPermutations2));
+                RaisePermutationException(nameof(getPermutations3));
             List<string> result = new List<string>();
             getPerm("", phrase, result);
             return result;
-
-
         }
 
         private static void getPerm(string prefix, string phrase, List<string> result)
@@ -87,6 +85,45 @@ namespace Chapter8
                 string character = phrase.Substring(i, 1);
                 getPerm(character + prefix, begin + end, result);
             }
+        }
+
+        public static List<string> getPermutationsIterative(string phrase)
+        {
+            if (phrase == null)
+                RaisePermutationException(nameof(getPermutationsIterative));
+
+            int size = phrase.Length;
+            List<string> perm = new List<string>();
+            perm.Add(phrase.Substring(0, 1));
+            phrase = phrase.Substring(1);
+
+            if (phrase == String.Empty)
+                return perm;
+
+            List<string> result = new List<string>();
+
+            foreach (char c in phrase)
+            {
+                result.Clear();
+                foreach (string str in perm)
+                {
+                    result.AddRange(InsertChar(c, str));
+                }
+                perm.Clear();
+                perm.AddRange(result);
+            }
+            
+            return perm;
+        }
+
+        private static List<string> InsertChar(char c, string str)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i <= str.Length; i++)
+            {
+                result.Add(new StringBuilder(str).Insert(i, c).ToString());
+            }
+            return result;
         }
 
         private static void RaisePermutationException(string method) => throw new ArgumentException($"{method}: The string to permutate must be not null");

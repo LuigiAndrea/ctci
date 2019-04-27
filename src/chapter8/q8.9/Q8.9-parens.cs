84255d2dca0ee6d6fe2883efff07af2efce90446
LAD <luigiandrea@gmail.com>
2019-04-27 17:23:38 -0400

    add Q8.9 without array to build the string

--- src/chapter8/q8.9/Q8.9-parens.cs
+++ src/chapter8/q8.9/Q8.9-parens.cs
@@ -6,10 +6,19 @@ namespace Chapter8
 {
     public static class Q8_9Parens
     {
+        private static char leftParens = '(';
+        private static char rightParens = ')';
         public static List<string> parentheses(int n)
         {
             List<string> list = new List<string>();
             parens(list, n, n, 0, new char[n * 2]);
+            return list;
+        }
+
+        public static List<string> parenthesesWithoutArray(int n)
+        {
+            List<string> list = new List<string>();
+            parens2(list, n, n, 0, new StringBuilder(new String('\t', n * 2)));
             return list;
         }
 
@@ -30,10 +39,27 @@ namespace Chapter8
             }
             else
             {
-                str[index] = '(';
+                str[index] = leftParens;
                 parens(list, left - 1, right, index + 1, str);
-                str[index] = ')';
+                str[index] = rightParens;
                 parens(list, left, --right, ++index, str);
+            }
+        }
+
+        private static void parens2(List<string> list, int left, int right, int index, StringBuilder str)
+        {
+            if (left > right || left < 0)
+                return;
+            if (right == 0 && left == 0)
+            {
+                list.Add(String.Concat(str));
+            }
+            else
+            {
+                addParen(index, str, leftParens);
+                parens2(list, left - 1, right, index + 1, str);
+                addParen(index, str, rightParens);
+                parens2(list, left, --right, ++index, str);
             }
         }
 
@@ -55,7 +81,7 @@ namespace Chapter8
                 {
                     if (paren[i] == '(')
                     {
-                        string strParen = insertParen(i, paren);
+                        string strParen = insertParens(i, paren);
                         combParens.Add(strParen);
                     }
                 }
@@ -66,7 +92,13 @@ namespace Chapter8
             return combParens;
         }
 
-        private static string insertParen(int i, string paren) => new StringBuilder(paren).Insert(i+1,"()").ToString();
-        
+        private static string insertParens(int i, string paren) => new StringBuilder(paren).Insert(i + 1, "()").ToString();
+
+        private static void addParen(int index, StringBuilder sb, char character)
+        {
+            sb.Remove(index, 1);
+            sb.Insert(index, character);
+        }
+
     }
 }

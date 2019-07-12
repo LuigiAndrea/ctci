d02e7662586a263d205a318c0482a69108da3f50
LAD <luigiandrea@gmail.com>
2019-07-12 00:32:45 -0400

    Improve performance using memorization for Q8.13

--- src/chapter8/q8.13/Q8.13-stack-boxes.cs
+++ src/chapter8/q8.13/Q8.13-stack-boxes.cs
@@ -7,7 +7,7 @@ namespace Chapter8
         public static int GetHeightsBoxes(Box[] boxes)
         {
             int maxHeight = 0;
-            Array.Sort(boxes,new Comparison<Box>((x,y)=>y.Height.CompareTo(x.Height)));         
+            Array.Sort(boxes, new Comparison<Box>((x, y) => y.Height.CompareTo(x.Height)));
 
             for (int i = 0; i < boxes.Length; i++)
             {
@@ -32,11 +32,50 @@ namespace Chapter8
                 }
             }
 
-            maxHeight+=bottomBox.Height;
+            maxHeight += bottomBox.Height;
             return maxHeight;
         }
 
-         public class Box {
+        public static int GetHeightsBoxesMemorization(Box[] boxes)
+        {
+            int maxHeight = 0;
+            Array.Sort(boxes, new Comparison<Box>((x, y) => y.Height.CompareTo(x.Height)));
+            int[] BoxHeighstMap = new int[boxes.Length];
+
+            for (int i = 0; i < boxes.Length; i++)
+            {
+                int height = buildStackOfBoxesMemorization(boxes, i, BoxHeighstMap);
+                maxHeight = Math.Max(height, maxHeight);
+            }
+
+            return maxHeight;
+        }
+
+        private static int buildStackOfBoxesMemorization(Box[] boxes, int bottomIndex, int[] boxHeightsMap)
+        {
+            if (boxHeightsMap[bottomIndex] > 0)
+            {
+                return boxHeightsMap[bottomIndex];
+            }
+
+            int maxHeight = 0;
+            Box bottomBox = boxes[bottomIndex];
+
+            for (int i = bottomIndex + 1; i < boxes.Length; i++)
+            {
+                if (boxes[i].CanGoAbove(bottomBox))
+                {
+                    int height = buildStackOfBoxesMemorization(boxes, i, boxHeightsMap);
+                    maxHeight = Math.Max(height, maxHeight);
+                }
+            }
+
+            maxHeight += bottomBox.Height;
+            return boxHeightsMap[bottomIndex] = maxHeight;
+        }
+
+        public class Box
+        {
             public int Width { get; set; }
             public int Height { get; set; }
             public int Depth { get; set; }

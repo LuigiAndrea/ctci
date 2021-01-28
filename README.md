95859885a96334989f3cc52913822002fd1c78bc
Luigi <luigiandrea@gmail.com>
2021-01-27 21:42:00 -0500

    refactor: heavy pill problem Q6.1

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ CTCI
 
 My solution to Cracking the Coding Interview 6th Edtion using C#, .NET Core Platform and Xunit
 
-Continuous work in progress
-
 ### Prerequisites
 
 You need to have installed .NET Core on your machine.
--- src/chapter6/q6.1/Q6.1-heavy-pill.cs
+++ src/chapter6/q6.1/Q6.1-heavy-pill.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Text;
 using static System.Console;
 
@@ -7,31 +8,39 @@ namespace Chapter6
     public static class Q6_1HeavyPill
     {
         private static double totalNormalWeight;
+        private static double totalSpecialWeight;
         private static double diffWeight;
-        private static ((double normalWeight, double specialWeight) pill, int count) bottle;
+        //  private static ((double normalWeight, double specialWeight) pill, int count) bottle;
+        //  private static List<Bottle> bottles;
 
-        public static void init(((double n, double s) p, int count) b)
+        public static void SetHeavyPillProblem(Pill p, int numberBottles, int heavyBottle)
         {
-            bottle = b;
-            diffWeight = bottle.pill.specialWeight - bottle.pill.normalWeight;
+            diffWeight = p.SpecialWeight - p.NormalWeight;
 
-            if (b.count < 2 & diffWeight==0)
-                throw new ArgumentException();        
-            
-            totalNormalWeight = (bottle.count * (bottle.count + 1) / 2) * bottle.pill.normalWeight;
-            WriteLine($"The bottle # {getBottle()} has the {(diffWeight>0 ? "heavy" : "light")} pills");
-        }
-        public static double getBottle()
-        {
-            return Math.Round((scale() - totalNormalWeight) / diffWeight, 0);
+            if (numberBottles < 2 || diffWeight == 0 || heavyBottle > numberBottles || heavyBottle < 1)
+                throw new ArgumentException();
+
+            int totalNumbePills = (numberBottles * (numberBottles + 1) / 2);
+            totalNormalWeight = totalNumbePills * p.NormalWeight;
+            totalSpecialWeight = totalNormalWeight + heavyBottle * diffWeight; // make the heavyBottle the one with the heavy pills         
         }
 
-        private static double scale()
+        //Get the bottle with heavier weight
+        public static double GetHeavyBottle() => (diffWeight != 0)
+        ? Math.Round((totalSpecialWeight - totalNormalWeight) / diffWeight, 0)
+        : throw new InvalidProgramException($"Call {nameof(SetHeavyPillProblem)} first");
+
+
+        public class Pill
         {
-            Random r = new Random();
-            int n = r.Next(1, bottle.count);
-            WriteLine($"Random bottle chosen: {n}");
-            return totalNormalWeight + n * diffWeight;
+            public double NormalWeight;
+            public double SpecialWeight;
+
+            public Pill(double nw, double sw)
+            {
+                NormalWeight = nw;
+                SpecialWeight = sw;
+            }
         }
     }
 }

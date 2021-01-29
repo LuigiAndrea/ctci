0299343c081c396351e8acb7104c36252ac738c7
Luigi <luigiandrea@gmail.com>
2021-01-29 01:59:07 -0500

    test: add exception test refactor: add new class HeavyPill

--- src/chapter6/q6.1/Q6.1-heavy-pill.cs
+++ src/chapter6/q6.1/Q6.1-heavy-pill.cs
@@ -4,29 +4,31 @@ namespace Chapter6
 {
     public static class Q6_1HeavyPill
     {
-        private static double totalNormalWeight;
-        private static double totalSpecialWeight;
-        private static double diffWeight;
-
-        public static void SetHeavyPillProblem(Pill p, int numberBottles, int heavyBottle)
+        public class HeavyPill
         {
-            diffWeight = p.SpecialWeight - p.NormalWeight;
+            private double totalNormalWeight;
+            private double totalSpecialWeight;
+            private double diffWeight;
 
-            if (numberBottles < 2 || diffWeight == 0
-            || heavyBottle > numberBottles || heavyBottle < 1 || p.SpecialWeight <= 0 || p.NormalWeight <= 0)
-                throw new ArgumentException();
+            public void SetHeavyPillProblem(Pill p, int numberBottles, int heavyBottle)
+            {
+                diffWeight = p.SpecialWeight - p.NormalWeight;
 
-            int totalNumbePills = (numberBottles * (numberBottles + 1) / 2);
-            totalNormalWeight = totalNumbePills * p.NormalWeight;
-            // Make the heavyBottle the one with the heavy pills         
-            totalSpecialWeight = totalNormalWeight + heavyBottle * diffWeight;
+                if (numberBottles < 2 || diffWeight == 0
+                || heavyBottle > numberBottles || heavyBottle < 1 || p.SpecialWeight <= 0 || p.NormalWeight <= 0)
+                    throw new ArgumentException();
+
+                int totalNumbePills = (numberBottles * (numberBottles + 1) / 2);
+                totalNormalWeight = totalNumbePills * p.NormalWeight;
+                // Make the heavyBottle the one with the heavy pills         
+                totalSpecialWeight = totalNormalWeight + heavyBottle * diffWeight;
+            }
+
+            //Get the bottle with pills with heavier weight
+            public double GetHeavyBottle() => (diffWeight != 0)
+            ? Math.Round((totalSpecialWeight - totalNormalWeight) / diffWeight, 0)
+            : throw new InvalidProgramException($"Call {nameof(SetHeavyPillProblem)} first");
         }
-
-        //Get the bottle with pills with heavier weight
-        public static double GetHeavyBottle() => (diffWeight != 0)
-        ? Math.Round((totalSpecialWeight - totalNormalWeight) / diffWeight, 0)
-        : throw new InvalidProgramException($"Call {nameof(SetHeavyPillProblem)} first");
-
 
         public class Pill
         {
--- test/chapter6/Q6.1-heavy-pill-test.cs
+++ test/chapter6/Q6.1-heavy-pill-test.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 using static Chapter6.Q6_1HeavyPill;
@@ -12,8 +13,32 @@ namespace Tests.Chapter6
         [InlineDataAttribute(1, 5, 20, 5)]
         private static void HeavyPillTest(double normalWeight, double specialWeight, int bottles, int heavyBottle)
         {
-            SetHeavyPillProblem(new Pill(normalWeight, specialWeight), bottles, heavyBottle);
-            Assert.Equal(heavyBottle, GetHeavyBottle());
+            HeavyPill hp = new HeavyPill();
+            hp.SetHeavyPillProblem(new Pill(normalWeight, specialWeight), bottles, heavyBottle);
+            Assert.Equal(heavyBottle, hp.GetHeavyBottle());
+        }
+
+
+        [TheoryAttribute]
+        [InlineDataAttribute(2.5, 1.2, 1, 13)]
+        [InlineDataAttribute(1.2, 1.2, 30, 15)]
+        [InlineDataAttribute(1, 5, 20, 55)]
+        [InlineDataAttribute(1, 5, 20, 0)]
+        [InlineDataAttribute(-1, 5, 20, 0)]
+        [InlineDataAttribute(1, 0, 20, 0)]
+        private static void HeavyPillExceptionTest(double normalWeight, double specialWeight, int bottles, int heavyBottle)
+        {
+            HeavyPill hp = new HeavyPill();
+            Exception ext = Record.Exception(() => hp.SetHeavyPillProblem(new Pill(normalWeight, specialWeight), bottles, heavyBottle));
+            Assert.IsType<ArgumentException>(ext);
+        }
+
+        [FactAttribute]
+        private static void HeavyPillExceptionWrongExecutionPathTest()
+        {
+            HeavyPill hp = new HeavyPill();
+            Exception ext = Record.Exception(() => hp.GetHeavyBottle());
+            Assert.IsType<InvalidProgramException>(ext);
         }
     }
 }

8b4efdd411bea494faf1f3188a677a24482daee7
Luigi <luigiandrea@gmail.com>
2021-02-05 01:32:55 -0500

    test: add tests for lines that intersect

--- test/chapter16/Q16.3-intersection-test.cs
+++ test/chapter16/Q16.3-intersection-test.cs
@@ -25,12 +25,21 @@ namespace Tests.Chapter6
     {
         public static TheoryData<Line, Line> geLinesWithIntersection()
         {
-            Point p = new Point(0, 0);
-            Line l = new Line(p, p);
-            return new TheoryData<Line, Line>() { { l, l } };
+            //vertical lines
+            Line lv1 = new Line(new Point(2, 4), new Point(2, 10));
+            Line lv2 = new Line(new Point(2, 7), new Point(2, 13));
 
+            //horizontal lines
+            Line lh1 = new Line(new Point(3, 5), new Point(5, 5));
+            Line lh2 = new Line(new Point(5, 5), new Point(7, 5));
 
+            //crossing lines
+            Line l1 = new Line(new Point(5, -2), new Point(1, -1));
+            Line l2 = new Line(new Point(-2, -5), new Point(2, -1));
+
+            return new TheoryData<Line, Line>() { { lv1, lv2 }, { lh1, lh2 }, { l1, l2 } };
         }
+
         public static TheoryData<Line, Line> geLinesWithoutIntersection()
         {
             Point p = new Point(0, 0);

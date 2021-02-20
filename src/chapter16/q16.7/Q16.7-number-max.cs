8682aed9d2aba87eb99f4890718a9b5ab3e2dd69
Luigi <luigiandrea@gmail.com>
2021-02-20 02:37:20 -0500

    feat: add Q16.7 max number

--- src/chapter16/q16.7/Q16.7-number-max.cs
+++ src/chapter16/q16.7/Q16.7-number-max.cs
@@ -1,13 +1,7 @@
-using System;
-
 namespace Chapter16
 {
     public class Q16_7NumberMax
     {
-        //Using Absolute Value
-        public static int GetMaxNumber(int a, int b)
-        {          
-           return -1;     
-        }
+        public static int GetMaxNumber(int a, int b) => ((a - b) & 0x8000) == 0 ? a : b;       
     }
 }

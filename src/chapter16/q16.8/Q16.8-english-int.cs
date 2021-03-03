026ace2bc7993b10f6b3db1c8664aa3db409c4f0
Luigi <luigiandrea@gmail.com>
2021-03-03 03:00:35 -0500

    feat: add Q16.8 english int problem

--- src/chapter16/q16.8/Q16.8-english-int.cs
+++ src/chapter16/q16.8/Q16.8-english-int.cs
@@ -1,10 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
 namespace Chapter16
 {
     public class Q16_8EnglishInt
     {
-        public static string GetEnglishInt(int number)
-        {  
-            return string.Empty;
-        }      
+        const int thousand = 1000;
+        static string[] special = new string[] { "", " Thousand", " Million", " Billion" };
+        static Dictionary<int, string> dic = new Dictionary<int, string>{
+            {0,"Zero"},{1," One"},{2," Two"},{3," Three"},{4," Four"},{5," Five"},{6," Six"},{7," Seven"},{8," Eight"},
+            {9," Nine"},{10," Ten"},{11," Eleven"},{12," Twelve"},{13," Thirteen"},{14," Fourteen"},{15," Fifteen"},
+            {16," Sixteen"},{17," Seventeen"},{18," Eighteen"},{19," Nineteen"},{20," Twenty"},{30," Thirty"},{40," Fourty"},
+            {50," Fifty"},{60," Sixty"},{70," Seventy"},{80," Eighty"},{90," Ninety"}
+            };
+
+        public static string GetEnglishInt(long number)
+        {
+            StringBuilder result = new StringBuilder();
+            bool neg = false;
+            if (number < 0)
+            {
+                neg = true;
+                number = -number;
+            }
+
+            if (number == 0)
+                return dic[0];
+
+            int i = 0;
+            while (number > 0)
+            {
+                var str = HandleTriplets((int)(number % thousand), i);
+                result.Insert(0, str);
+                number /= thousand;
+                i++;
+            }
+
+            if (neg)
+            {
+                result.Insert(0, "Negative");
+            }
+
+            return result.ToString().Trim();
+        }
+
+        private static StringBuilder HandleTriplets(int n, int i)
+        {
+            StringBuilder str = new StringBuilder();
+
+            if (n == 0)
+                return str;
+
+            int firstNninteen = n % 100;
+            if (firstNninteen < 20 && firstNninteen > 0)
+            {
+                str.Insert(0, dic[(firstNninteen)]);
+                n /= 100;
+            }
+            else
+            {
+                if (n % 10 > 0)
+                {
+                    str.Insert(0, dic[(n % 10)]);
+                }
+
+                n /= 10;
+                if (n % 10 > 0)
+                {
+                    str.Insert(0, dic[(n % 10) * 10]);
+                }
+                n /= 10;
+            }
+
+            if (n % 10 > 0)
+            {
+                str.Insert(0, dic[(n % 10)] + " Hundred");
+            }
+
+            return str.Append(special[i]);
+        }
     }
 }
--- test/chapter16/Q16.8-english-int-test.cs
+++ test/chapter16/Q16.8-english-int-test.cs
@@ -7,13 +7,21 @@ namespace Tests.Chapter6
     public class Q16_8
     {
         [TheoryAttribute]
+        [InlineDataAttribute("Nine Hundred Ninety Nine Billion Nine Hundred Ninety Nine Million Nine Hundred Ninety Nine Thousand Nine Hundred Ninety Nine", 999999999999)] //Max number allowed
+        [InlineDataAttribute("Thirty Three Billion", 33000000000)]
         [InlineDataAttribute("Negative Five Hundred Two", -502)]
-        [InlineDataAttribute("One Billion Two Hundred Twenty Milion Fifty Thousand One Hundred Eleven", 1220050111)]
-        [InlineDataAttribute("Nine Hundred Ninty Nine Milion Two Thousand Thirteen", 999002013)]
+        [InlineDataAttribute("One Billion Two Hundred Twenty Million Fifty Thousand One Hundred Eleven", 1220050111)]
+        [InlineDataAttribute("Nine Hundred Ninety Nine Million Two Thousand Thirteen", 999002013)]
         [InlineDataAttribute("Five Hundred Fifty Five Thousand One", 555001)]
         [InlineDataAttribute("Two Hundred Four", 204)]
+        [InlineDataAttribute("Nineteen Million Four Hundred Seven Thousand Nine", 19407009)]
+        [InlineDataAttribute("Sixty Two Million Thirty", 62000030)]
+        [InlineDataAttribute("Eighty One Thousand", 81000)]
+        [InlineDataAttribute("Negative Seventy Seven Thousand One Hundred", -77100)]
+        [InlineDataAttribute("Seventy Seven Thousand One Hundred One", 77101)]
+        [InlineDataAttribute("Zero", 0)]
         [InlineDataAttribute("Seven", 7)]
-        public void EnglishIntTest(string sentence, int num)
+        public void EnglishIntTest(string sentence, long num)
         {
             Assert.Equal(sentence, GetEnglishInt(num));
         }

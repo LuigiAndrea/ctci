f46cb12b92b8bc19dd7a5b74012e1e631894f1fb
Luigi <luigiandrea@gmail.com>
2021-03-19 18:54:13 -0400

    feat: add divide method, test and refactor code

--- src/chapter16/q16.9/Q16.9-operations.cs
+++ src/chapter16/q16.9/Q16.9-operations.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Chapter16
 {
     public class Q16_8Operations
@@ -5,26 +7,12 @@ namespace Chapter16
         public static int Multiply(int a, int b)
         {
             int res = 0;
-            int sign = 1;
 
-            if (a < 0)
-            {
-                sign = -1;
-                a = -a;
-            }
+            int sign = getSign(a);
+            sign *= getSign(b);
 
-            if (b < 0)
-            {
-                sign = -sign;
-                b = -b;
-            }
-
-            if (a > b)
-            {
-                int temp = b;
-                b = a;
-                a = temp;
-            }
+            makePositive(ref a, ref b);
+            swap(ref a, ref b);
 
             for (int i = 0; i < a; i++)
             {
@@ -33,13 +21,52 @@ namespace Chapter16
             return sign * res;
         }
 
+        public static int Divide(int a, int b)
+        {
+            int res = 0;
+
+            if(b==0){
+                  throw new ArgumentException("Please provide a number greater than 0 for the second parameter");
+            }
+
+            int sign = getSign(a);
+            sign *= getSign(b);
+
+            makePositive(ref a, ref b);
+            int cond=b;
+            while(a >= cond)
+            {
+                cond+=b;
+                res++;
+            }
+
+            return sign*res;
+        }
+
         public static int Subtract(int a, int b)
         {
             return 0;
         }
-        public static int Divide(int a, int b)
+
+        //Return 1 if the sign is positive +, -1 otherwise -
+        private static int getSign(int n) => (n < 0) ? -1 : 1;
+
+        //Make a and b positive numbers
+        private static void makePositive(ref int a, ref int b)
         {
-            return 0;
+            a = (a < 0) ? -a : a;
+            b = (b < 0) ? -b : b;
         }
+
+        //swap a and b in order to have a < b 
+        private static void swap(ref int a, ref int b){
+            if (a > b)
+            {
+                int temp = b;
+                b = a;
+                a = temp;
+            }
+        }
+
     }
 }
--- test/chapter16/Q16.9-operations-test.cs
+++ test/chapter16/Q16.9-operations-test.cs
@@ -1,3 +1,4 @@
+using System;
 using Xunit;
 
 using static Chapter16.Q16_8Operations;
@@ -16,6 +17,31 @@ namespace Tests.Chapter16
         public void MultiplyTest(int a, int b, int result)
         {
             Assert.Equal(result, Multiply(a, b));
+        }
+
+        [TheoryAttribute]
+        [InlineDataAttribute(9, 11, 0)]
+        [InlineDataAttribute(12, 11, 1)]
+        [InlineDataAttribute(0, 10, 0)]
+        [InlineDataAttribute(20, 2, 10)]
+        [InlineDataAttribute(20, 3, 6)]
+        [InlineDataAttribute(-20, 2, -10)]
+        [InlineDataAttribute(20, -3, -6)]
+        [InlineDataAttribute(-20, -2, 10)]
+        [InlineDataAttribute(-20, -3, 6)]
+        [InlineDataAttribute(-3, 3, -1)]
+        public void DivideTest(int a, int b, int result)
+        {
+            Assert.Equal(result, Divide(a, b));
+        }
+
+        [TheoryAttribute]
+        [InlineDataAttribute(5, 0)]
+        [InlineDataAttribute(-12, 0)]
+        private void DivideExceptionTest(int a, int b)
+        {
+            Exception actualException = Record.Exception(() => Divide(a, b));
+            Assert.IsType<ArgumentException>(actualException);
         }
     }
 }

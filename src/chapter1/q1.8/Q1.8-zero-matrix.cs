76601d7d810bd7f343c5d51364f2dfcd675f714f
Luigi <luigiandrea@gmail.com>
2021-04-07 02:11:13 -0400

    feat: add Q1.8 zero matrix

--- src/chapter1/q1.8/Q1.8-zero-matrix.cs
+++ src/chapter1/q1.8/Q1.8-zero-matrix.cs
@@ -1,22 +1,51 @@
 namespace Chapter1
 {
     using System;
-    using System.Text;
+    using System.Collections.Generic;
 
     public class Q1_8ZeroMatrix
     {
-        public static void ZeroMatrix(int[,] matrix)
+        public static void ZeroMatrix(int[,] mtx)
         {
-            if (matrix == null || matrix.GetLength(0) == 0)
+
+            if (mtx == null || mtx.GetLength(0) == 0)
             {
                 throw new ArgumentException("Please provide a non empty MxN matrix");
             }
+
+            (int rows, int cols) dim = (mtx.GetLength(0), mtx.GetLength(1));
+            List<(int r, int c)> zeros = new List<(int r, int c)>();
+
+            for (int i = 0; i < dim.rows; i++)
+            {
+                for (int j = 0; j < dim.cols; j++)
+                {
+                    if (mtx[i, j] == 0)
+                    {
+                        zeros.Add((i, j));
+                    }
+                }
+            }
+
+            foreach (var p in zeros)
+            {
+                zeroColAndRow(dim, (p.r, p.c), mtx);
+            }
         }
 
-        public static void zeroColAndRow(int[,] mtx)
+        public static void zeroColAndRow((int r, int c) dim, (int r, int c) pos, int[,] mtx)
         {
+            //zero the cols
+            for (int i = 0; i < dim.c; i++)
+            {
+                mtx[pos.r, i] = 0;
+            }
 
+            //zero the rows
+            for (int i = 0; i < dim.r; i++)
+            {
+                mtx[i, pos.c] = 0;
+            }
         }
-
     }
 }
--- test/chapter1/Q1.8-zero-matrix-test.cs
+++ test/chapter1/Q1.8-zero-matrix-test.cs
@@ -14,18 +14,47 @@ namespace Tests.Chapter1
             int[,] expected = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
             ZeroMatrix(matrix);
+            Assert.Equal(expected, matrix);
 
             matrix = new int[,] { { 1, 2, 3 }, { 4, 0, 6 }, { 7, 8, 9 } };
             expected = new int[,] { { 1, 0, 3 }, { 0, 0, 0 }, { 7, 0, 9 } };
             ZeroMatrix(matrix);
-
             Assert.Equal(expected, matrix);
 
             matrix = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 0 } };
-            expected = new int[,] { { 0, 2, 0 }, { 4, 0, 0 }, { 7, 8, 0 } };
+            expected = new int[,] { { 1, 2, 0 }, { 4, 5, 0 }, { 0, 0, 0 } };
             ZeroMatrix(matrix);
-
             Assert.Equal(expected, matrix);
+        }
+
+        [FactAttribute]
+        public void MoreZeroMatrixTest()
+        {
+            int[,] matrix = new int[,] { { 0, 2, 0 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            int[,] expected = new int[,] { { 0, 0, 0 }, { 0, 5, 0 }, { 0, 8, 0 } };
+
+            ZeroMatrix(matrix);
+            Assert.Equal(expected, matrix);
+        }
+
+        [FactAttribute]
+        public void ZeroMatrixDiffDimensionsTest()
+        {
+            int[,] matrix = new int[,] { { 1, 0, 3 }, { 4, 5, 6 }, { 7, 8, 9 }, { 10, 11, 12 } };
+            int[,] expected = new int[,] { { 0, 0, 0 }, { 4, 0, 6 }, { 7, 0, 9 }, { 10, 0, 12 } };
+
+            ZeroMatrix(matrix);
+            Assert.Equal(expected, matrix);
+        }
+
+        [FactAttribute]
+        public void ZeroMatrixExceptionTest()
+        {
+            Exception ex = Record.Exception(() => ZeroMatrix(new int[,] { }));
+            Assert.IsType<ArgumentException>(ex);
+
+            ex = Record.Exception(() => ZeroMatrix(null));
+            Assert.IsType<ArgumentException>(ex);
         }
     }
 }
